a0a247fc380f4652bdd46915e203e1fb8470e2e6
jesse10930 <ronda.jesse@gmail.com>
2021-06-29 11:45:06 -0400

    refactoring code

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -1,18 +1,23 @@
-// Hamburger Menu
-let hamMenuOpen = document.getElementById('three-lines');
-let hamMenuClose = document.getElementById('close-btn');
-let hamModal = document.getElementById('my-hamburger-modal');
+(function () {
+  // Hamburger Menu
+  let hamMenuOpen = document.getElementById('three-lines');
+  let hamMenuClose = document.getElementById('close-btn');
+  let hamModal = document.getElementById('my-hamburger-modal');
 
-hamMenuOpen.onclick = (e) => {
-  hamMenuOpen.style.display = 'none';
-  hamMenuClose.style.display = 'flex';
-  document.querySelector('body').style.overflow = 'hidden';
-  hamModal.style.display = 'inline';
-};
+  showDropdown = (e) => {
+    hamMenuOpen.style.display = 'none';
+    hamMenuClose.style.display = 'flex';
+    document.querySelector('body').style.overflow = 'hidden';
+    hamModal.style.display = 'inline';
+  };
 
-hamMenuClose.onclick = (e) => {
-  hamMenuOpen.style.display = 'flex';
-  hamMenuClose.style.display = 'none';
-  document.querySelector('body').style.overflow = 'visible';
-  hamModal.style.display = 'none';
-};
+  hideDropdown = (e) => {
+    hamMenuOpen.style.display = 'flex';
+    hamMenuClose.style.display = 'none';
+    document.querySelector('body').style.overflow = 'visible';
+    hamModal.style.display = 'none';
+  };
+
+  hamMenuOpen.onclick = () => showDropdown();
+  hamMenuClose.onclick = () => hideDropdown();
+})();
